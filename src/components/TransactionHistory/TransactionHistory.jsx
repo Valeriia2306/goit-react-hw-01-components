b16c27f29d82526de1bf1fd8ee5dba2004c18b10
Valeriia Frolova <valeriiats1989@gmail.com>
2023-02-20 23:22:40 +0200

    fix

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-// style Component
+
 import {
   Table,
   Header,
@@ -10,7 +10,6 @@ import {
   TransactionLine,
 } from './TransactionHistory.styled';
 
-// utils
 import { normalizeType } from '../utilis/normalizeType';
 
 function TransactionTable({ items }) {
